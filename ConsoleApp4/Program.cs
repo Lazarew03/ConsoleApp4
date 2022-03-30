9dc5205f3d8201531722a6f7a810d99c7a8e27f2
user <user@C1009>
2022-03-30 13:35:45 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+var numsList = new List<int> { 11,22,33,44,55,66,77,88,99};
+foreach (var salmon in numsList)
+{
+    Console.Write(salmon + " " );
+
+}
+Console.WriteLine(" - Элементы коллекции");
+
+
+foreach (var salmon in numsList)
+{
+	
+	for (int i = 0; i < 9; i++)
+    {
+		int sum = 0;
+        
+        sum += (int)(numsList[i] / 10);
+        sum += numsList[i] % 10;
+        int res = sum;
+Console.WriteLine(res);
+    }
+        Console.ReadLine();
+    
+    
+
+}
+   
+
+
+
